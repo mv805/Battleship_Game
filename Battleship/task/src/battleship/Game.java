f71f37276ca43e943a2ae4086e22ab923cb29e04
Matt Villa <matthewvilla805@gmail.com>
2022-01-28 15:08:47 -0800

    Add check to make sure the input is the right distance for the ship placement prompt

--- Battleship/task/src/battleship/Game.java
+++ Battleship/task/src/battleship/Game.java
@@ -1,5 +1,6 @@
 package battleship;
 
+import java.awt.geom.Point2D;
 import java.util.Scanner;
 
 public class Game {
@@ -42,15 +43,14 @@ public class Game {
 
         for (Ship ship: gameShips) {
             int[] shipCoord;
+
+            //prompt for input
+            promptShipPlacement(ship);
             do {
-
-                //prompt for input
-                promptShipPlacement(ship);
-
                 //define the ship coordinates
                 shipCoord = inputToIntArray();
 
-            }while (!checkValidShipCoord(shipCoord));
+            }while (!checkValidShipCoord(shipCoord, ship));
             
             //hand coordinates to board to place
             ship.recordShipCoord(shipCoord);
@@ -66,9 +66,16 @@ public class Game {
                 ship.getType(), ship.getShipStatus().length);
     }
 
-    private boolean checkValidShipCoord(int[] coordinates){
+    private boolean checkValidShipCoord(int[] coordinates, Ship ship){
+        //input for A1 to A5 would be 0 0 0 4 for example...
+        // or A1 to E1 0 0 4 0
 
-        //check if valid
+        //check if valid distance
+        if (ship.getShipStatus().length != twoPointDistance(coordinates)){
+            System.out.printf("Error! Wrong length of the %s! Try again:%n%n",
+                    ship.getType());
+            return false;
+        }
         //logic to : check for diagonal, check for too close, check for long
         //enough, check for not crossing
         //print appropriate error if false
@@ -76,7 +83,8 @@ public class Game {
     }
 
     int[] inputToIntArray(){
-
+        //recieve input per grid letter and number format and returns it in
+        //an index format for array (and in an array)
         final int aValue = 65;
         int[] intInput = new int[4];
         int index = 0;
@@ -87,11 +95,13 @@ public class Game {
             intInput[0] = (int) (stringInput[0].charAt(0)) - aValue;
             intInput[1] = board.gameBoard[0].length - 1;
             intInput[2] = (int) (stringInput[1].charAt(0)) - aValue;
-            intInput[3] = Character.getNumericValue(stringInput[1].charAt(1)) - 1;
+            intInput[3] = Character.getNumericValue(stringInput[1]
+                    .charAt(1)) - 1;
             return intInput;
         } else if (stringInput[1].length() > 2 && stringInput[0].length() == 2){
             intInput[0] = (int) (stringInput[0].charAt(0)) - aValue;
-            intInput[1] = Character.getNumericValue(stringInput[0].charAt(1)) - 1;
+            intInput[1] = Character.getNumericValue(stringInput[0]
+                    .charAt(1)) - 1;
             intInput[2] = (int) (stringInput[1].charAt(0)) - aValue;
             intInput[3] = board.gameBoard[0].length - 1;
             return intInput;
@@ -103,10 +113,26 @@ public class Game {
             return intInput;
         } else {
             intInput[0] = (int) (stringInput[0].charAt(0)) - aValue;
-            intInput[1] = Character.getNumericValue(stringInput[0].charAt(1)) - 1;
+            intInput[1] = Character.getNumericValue(stringInput[0]
+                    .charAt(1)) - 1;
             intInput[2] = (int) (stringInput[1].charAt(0)) - aValue;
-            intInput[3] = Character.getNumericValue(stringInput[1].charAt(1)) - 1;
+            intInput[3] = Character.getNumericValue(stringInput[1]
+                    .charAt(1)) - 1;
             return intInput;
         }
     }
+
+    int twoPointDistance(int[] array){
+
+        // distance between two points stored in 4 digit array
+        // with format X1, Y1, X2, Y2
+
+        int x2 = array[2];
+        int y2 = array[3];
+        int x1 = array[0];
+        int y1 = array[1];
+
+        return (int) Point2D.distance(x1, y1, x2, y2) + 1;
+    }
+
 }
